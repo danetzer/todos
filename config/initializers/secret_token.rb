ae0e98e5247d0f7e721fc2b6843d3e5fd19f2421
dan <dan@capricalabs.com>
2015-03-16 10:04:22 -0700

    added rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todos::Application.config.secret_key_base = 'b9eca1939e0ed1f7b226fae63899f61edd683a56ac99d97018a2693a5f694facf333a13e906013ff250e1ff66028cb7e4951fa8de494b12bd58df3e3cad2d7f4'

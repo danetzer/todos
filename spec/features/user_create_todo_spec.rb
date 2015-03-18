f454e2398dd6c57a583cf2bc8139d5bc0f6ef6c7
dan <dan@capricalabs.com>
2015-03-18 16:19:51 -0700

    refactored tests to use helpers

--- spec/features/user_create_todo_spec.rb
+++ spec/features/user_create_todo_spec.rb
@@ -4,11 +4,8 @@ feature "User creates todo" do
   scenario "successfully" do
     sign_in
 
-    click_on "Add a new todo"
+    create_todo("Buy milk")
 
-    fill_in "Title", with: "Buy milk"
-    click_on "Submit"
-    
-    expect(page).to have_css '.todos li', text: "Buy milk"
+    expect(page).to display_todo("Buy milk")
   end
 end
